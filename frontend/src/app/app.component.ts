a429ae0fbe45e5891e72c58450e0bc94e903e286
Bjorn S <bjorn@somnu.com>
2017-10-10 17:39:17 +0200

    Combines frontend and api into 1 docker.

--- frontend/src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import {AlertsService} from './shared/alerts.service'
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {}
--- frontend/src/app/base/menu/menu.component.ts
+++
@@ -1,8 +0,0 @@
-import { HttpClient } from '@angular/common/http'
-import {Component} from '@angular/core'
-
-@Component({
-    selector: 'menubar',
-    templateUrl: './menu.html'
-})
-export class MenuComponent {}
